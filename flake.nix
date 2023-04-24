7c43b97bc152c20ce2d9396d7e1cc6db07c8c1cc
Samuel Rounce <me@samuelrounce.co.uk>
2023-04-24 01:02:34 +0100

    fix: use project rust version for linting/formatting

--- flake.nix
+++ flake.nix
@@ -81,6 +81,7 @@
             projectRootFile = "flake.nix";
             programs.nixpkgs-fmt.enable = true;
             programs.rustfmt.enable = true;
+            programs.rustfmt.package = rustVersion;
           };
         };
     };
